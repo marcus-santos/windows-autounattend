68431d0ceb5dce5a50fd1b67d4bd707104bc5749
Marcus Santos <marcus@marcus-santos.com>
2022-09-08 14:41:50 -0300

    Update chocolatey-apps.cmd

--- chocolatey-apps.cmd
+++ chocolatey-apps.cmd
@@ -1,25 +1,9 @@
 @ECHO OFF
 set CHOCO=%systemdrive%\ProgramData\Chocolatey\choco.exe
-set CI=%systemdrive%\ProgramData\Chocolatey\choco.exe install
+set CI=%systemdrive%\ProgramData\Chocolatey\choco.exe install -y
 
 %CI% chocolateygui
 %CI% 7zip
-%CI% flashplayerplugin
 %CI% googlechrome
-%CI% jre8
-%CI% foxitreader
-%CI% cdburnderxp
-%CI% imgburn
-%CI% deluge
-%CI% paint.net
-%CI% notepad2
-%CI% vlc
-%CI% ccleaner
-%CI% filezilla
-%CI% keepass.install
-%CI% libreoffice
-%CI% atom
-%CI% notepad2
-%CI% notepadplusplus
-%CI% gimp
-%CI% owncloud-client
+%CI% firefox
+%CI% Boxstarter
